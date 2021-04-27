6c96a505162a5e1eebc3e09fdf0396f9003f9ba3
sunil448832 <sunil4832sharma@gmail.com>
2021-04-28 04:38:48 +0530

    initial Commit

--- 05_Anomaly_Detection/autoencoder.py
+++ 05_Anomaly_Detection/autoencoder.py
@@ -4,77 +4,86 @@ import torchvision
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.utils.data import Dataset,DataLoader
+from torch.utils.data import Dataset, DataLoader
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 class AutoEncoder(nn.Module):
-  def __init__(self):
-    super(AutoEncoder, self).__init__()
-    pool = nn.MaxPool2d(kernel_size=2, stride=2) 
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-    #Encoder
-    self.encoder=nn.Sequential(nn.Conv2d(3, 8, kernel_size=3,stride=1,padding=1),nn.ReLU(inplace=True),pool,
-                               nn.Conv2d(8, 16, kernel_size=3,stride=1,padding=1),nn.ReLU(inplace=True),pool,
-                               nn.Conv2d(16, 32, kernel_size=3,stride=1,padding=1),nn.ReLU(inplace=True),pool,
-                               )
-    
-    #Decoder
-    self.decoder=nn.Sequential(nn.ConvTranspose2d(32, 16,kernel_size=2, stride=2),nn.ReLU(inplace=True),
-                               nn.ConvTranspose2d(16, 8,kernel_size=2, stride=2),nn.ReLU(inplace=True),
-                               nn.ConvTranspose2d(8, 3,kernel_size=2, stride=2),nn.Sigmoid()
-                               )
-    self.criterion=nn.MSELoss()
+        # Encoder
+        self.encoder = nn.Sequential(nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True), pool,
+                                     nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1), nn.ReLU(
+                                         inplace=True), pool,
+                                     nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1), nn.ReLU(
+                                         inplace=True), pool,
+                                     )
 
-  def forward(self, x):
-    x=self.encoder(x)
-    x=self.decoder(x)   
-    return x
-    
-  # Auto Encoder loss function  
-  def loss(self,x,y):
-    x=x.view(x.size(0),-1).float()
-    y=y.view(y.size(0),-1).float()
-    return self.criterion(x,y)
+        # Decoder
+        self.decoder = nn.Sequential(nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2), nn.ReLU(inplace=True),
+                                     nn.ConvTranspose2d(
+                                         16, 8, kernel_size=2, stride=2), nn.ReLU(inplace=True),
+                                     nn.ConvTranspose2d(
+                                         8, 3, kernel_size=2, stride=2), nn.Sigmoid()
+                                     )
+        self.criterion = nn.MSELoss()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+    # Auto Encoder loss function
+    def loss(self, x, y):
+        x = x.view(x.size(0), -1).float()
+        y = y.view(y.size(0), -1).float()
+        return self.criterion(x, y)
 
 
-def train(model,train_data,optimizer,batch_size):
-    
-  epoch_loss = 0
-  len=0
-  model.train()
-  for input,target in tqdm(DataLoader(train_data,batch_size=batch_size)):
-    optimizer.zero_grad()
-    input,target= input.to(device),target.to(device)
-    predictions = model(input)
-    loss = model.loss(predictions,target)
+def train(model, train_data, optimizer, batch_size):
 
-    loss.backward()
-    optimizer.step()
+    epoch_loss = 0
+    len = 0
+    model.train()
+    for input, target in tqdm(DataLoader(train_data, batch_size=batch_size)):
+        optimizer.zero_grad()
+        input, target = input.to(device), target.to(device)
+        predictions = model(input)
+        loss = model.loss(predictions, target)
 
-    epoch_loss += loss.item()
-    len+=1
-      
-  return epoch_loss / len
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        len += 1
+
+    return epoch_loss / len
 
 # function to evaluate the model on validation dataset returns loss and accuracy
-def eval(model,eval_data,batch_size):
-    
-  epoch_loss = 0
-  len=0
-  model.eval()
-  with torch.no_grad():
-    for input,target in tqdm(DataLoader(eval_data,batch_size=batch_size)):
-      input,target= input.to(device),target.to(device)
-      predictions = model(input)
-      loss = model.loss(predictions,target)
-      epoch_loss += loss.item()
-      len+=1 
-      
-  return epoch_loss / len
 
-def fit(model,train_data,val_data,epoch,lr=.001):
-  optimizer = optim.Adam(model.parameters(),lr=.01)
-  for epoch in range(epoch):
-    train_loss=train(model,train_data,optimizer,64)
-    val_loss=eval(model,val_data,64)
-    print("epoch: %d  train_loss: %.4f  val_loss: %.4f"%(epoch,train_loss,val_loss))
+
+def eval(model, eval_data, batch_size):
+
+    epoch_loss = 0
+    len = 0
+    model.eval()
+    with torch.no_grad():
+        for input, target in tqdm(DataLoader(eval_data, batch_size=batch_size)):
+            input, target = input.to(device), target.to(device)
+            predictions = model(input)
+            loss = model.loss(predictions, target)
+            epoch_loss += loss.item()
+            len += 1
+
+    return epoch_loss / len
+
+
+def fit(model, train_data, val_data, epoch, lr=.001):
+    optimizer = optim.Adam(model.parameters(), lr=.01)
+    for epoch in range(epoch):
+        train_loss = train(model, train_data, optimizer, 64)
+        val_loss = eval(model, val_data, 64)
+        print("epoch: %d  train_loss: %.4f  val_loss: %.4f" %
+              (epoch, train_loss, val_loss))
--- 05_Anomaly_Detection/dataset.py
+++ 05_Anomaly_Detection/dataset.py
@@ -4,58 +4,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torchvision
-from torch.utils.data import Dataset,DataLoader
-
+from torch.utils.data import Dataset, DataLoader
 
 
 def get_frames(video):
-  spatial,temporal=[],[]
-  frames=sorted(glob.glob(video+"/*"))
-  for i in range(len(frames)):
-    spatial.append(frames[i])
-    if i-1>=0:temporal.append((frames[i],frames[i-1]))
-    if i-2>=0:temporal.append((frames[i],frames[i-2]))
-    if i-3>=0:temporal.append((frames[i],frames[i-3]))
-    if i-4>=0:temporal.append((frames[i],frames[i-4]))
-  return spatial,temporal
+    spatial, temporal = [], []
+    frames = sorted(glob.glob(video+"/*"))
+    for i in range(len(frames)):
+        spatial.append(frames[i])
+        if i-1 >= 0:
+            temporal.append((frames[i], frames[i-1]))
+        if i-2 >= 0:
+            temporal.append((frames[i], frames[i-2]))
+        if i-3 >= 0:
+            temporal.append((frames[i], frames[i-3]))
+        if i-4 >= 0:
+            temporal.append((frames[i], frames[i-4]))
+    return spatial, temporal
+
 
 def get_path(dir):
-  spatial,temporal=[],[]
-  videos=glob.glob(dir+"/*")
-  for vid in videos:
-    sp,temp=get_frames(vid)
-    spatial.extend(sp)
-    temporal.extend(temp)
-  return spatial,temporal
+    spatial, temporal = [], []
+    videos = glob.glob(dir+"/*")
+    for vid in videos:
+        sp, temp = get_frames(vid)
+        spatial.extend(sp)
+        temporal.extend(temp)
+    return spatial, temporal
 
 
 def preprocess(img):
-  img=cv2.resize(img,(256,128))
-  img = torch.Tensor(img)
-  return img.permute(2,0,1).float()/255.0
+    img = cv2.resize(img, (256, 128))
+    img = torch.Tensor(img)
+    return img.permute(2, 0, 1).float()/255.0
+
 
 class MyDataset(Dataset):
-  def __init__(self,path,spatial=True):
-    super(MyDataset,self).__init__()
-    self.dataset=path
-    self.spatial=spatial
-    self.transform=preprocess
+    def __init__(self, path, spatial=True):
+        super(MyDataset, self).__init__()
+        self.dataset = path
+        self.spatial = spatial
+        self.transform = preprocess
 
-  def __getitem__(self,index):
-    if self.spatial:
-      image=cv2.imread(self.dataset[index])
-      image=self.transform(image)
-      return image,image
-    else:
-      img1,img2=self.dataset[index]
-      image_diff=cv2.imread(img1)-cv2.imread(img2)
-      image_diff=self.transform(image_diff)
-      return image_diff,image_diff
+    def __getitem__(self, index):
+        if self.spatial:
+            image = cv2.imread(self.dataset[index])
+            image = self.transform(image)
+            return image, image
+        else:
+            img1, img2 = self.dataset[index]
+            image_diff = cv2.imread(img1)-cv2.imread(img2)
+            image_diff = self.transform(image_diff)
+            return image_diff, image_diff
 
-  def __len__(self):
-    return len(self.dataset)
+    def __len__(self):
+        return len(self.dataset)
+
 
 def show(img):
-  plt.figure(figsize=(50,100)) 
-  npimg = img.numpy()
-  plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
+    plt.figure(figsize=(50, 100))
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)), interpolation='nearest')
